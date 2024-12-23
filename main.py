2f7612eb43e5ee71e2ad960cf45ff42bb93189a6
nja <naycro@hotmail.fr>
2024-12-23 20:04:41 +0100

    B: build a word count

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+with open("books/frankenstein.txt") as f:
+    text = f.read()
+    words = count_words(text)
+    print(words)
