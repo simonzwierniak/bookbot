8766aa6f4a4405596654b6abb3f68e05efd4b115
nja <naycro@hotmail.fr>
2024-12-27 17:39:29 +0100

    D: Add char_count

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
         text = f.read()
         words = count_words(text)
         lower_words = get_lower_words(text)
+        char_count = get_char_count(lower_words)
         print(words)
-        print(lower_words)
+        print(char_count)
+        ## print(lower_words)
 
 def count_words(text):
     words = text.split()
@@ -13,6 +15,14 @@ def count_words(text):
 def get_lower_words(text):
     lower_words = text.lower()
     return lower_words
+
+def get_char_count(text):
+    char_count = {}
+    for char in text.lower():
+        if char.isalpha():
+            char_count[char] = char_count.get(char, 0) + 1
+    return char_count
+
 
 if __name__ == "__main__":
     main()
